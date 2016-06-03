b01d16777188481e6f5fc6c5149222ee88a47110
dolomitt <dolomitt@gmail.com>
2016-06-03 14:32:09 +0800

    Changing Versions to Latest - Removing unnecessary files

--- transporter/mongo-es.js
+++
@@ -1,5 +0,0 @@
-pipeline = Source({name:"mongo", tail: true, namespace: "harvester-test.entries"})
-  .transform({filename: "transformers/passthrough_and_log.js", namespace: "harvester-test.entries"})
-  .save({name:"es", namespace: "harvester-test.entries"})
-
-
